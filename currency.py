85ff6200fa6137f890441bbd05d69dc80f6ab4bd
hanfoldo <88119937+hanfoldo@users.noreply.github.com>
2023-02-03 20:57:11 +0000

    if else loop demo code

---
+++ currency.py
@@ -0,0 +1,9 @@
+china = input('how much yuan do you have: ')
+
+japan = input('how many yen do you have: ')
+
+korea = input('how much won do you have: ')
+
+money = (int)(china) + (int)(korea) + (int)(japan)
+
+print("The total money you have is " + (str)(money))
