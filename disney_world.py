3ce797770abdccc1db3ffeb36bb89fbbee34f3cd
hanfoldo <88119937+hanfoldo@users.noreply.github.com>
2023-02-24 02:12:42 +0000

    Disney Top Rides selection

---
+++ disney_world.py
@@ -0,0 +1,39 @@
+print('where are you going in disney world')
+
+print('1. animal kingdom')
+print('2. epcot')
+print('3. magic kingdom')
+print('4. hollywood studios')
+ 
+dw = int(input('enter you answer (1-4): '))
+
+if dw == 1:
+    print('here are the best rides for you')
+    print('5. Kali river rapids')
+    print('4.Navi river journy')
+    print('3. Dinosuar')
+    print('2.expedition everest')
+    print('1. avatar flight of passage')
+elif dw == 2:
+    print('here are the best rides for you')
+    print('5. gran fiesta tour')
+    print('4. living with the land')
+    print('3. sorin around the world')
+    print('2. test track')
+    print('1. space ship earth')
+elif dw == 3:
+    print('here are the best rides for you')
+    print('5. people mover ')
+    print('4. splash mountain')
+    print('3.big thunder mountain railroad')
+    print('2. seven dwarf mine trains')
+    print('1. space mountain')
+elif dw == 4:
+    print('here are the top 5 rides for you')
+    print('5. star tours ')
+    print('4. sslinky dog dash')
+    print('3. twlight zone tower of terror')
+    print('2. rise of the risistence')
+    print('1.melliniom fallcon smugglers run')
+else:
+    print('wrong input, can not process')
